0c98406246dc07c72bb3f25dcb66a86c6f6ff6cd
SaekiTominaga <kcsu3470+github@gmail.com>
2021-05-21 12:25:14 +0900

    import 先変換ミス修正

--- dist/InputIsbn.js
+++ dist/InputIsbn.js
@@ -12,7 +12,7 @@ var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (
     return privateMap.get(receiver);
 };
 var _checkDigitMessage, _formSubmitEventListener;
-import IsbnVerify from '@saekitominaga/isbn-verify';
+import IsbnVerify from '../../isbn-verify/dist/ISBN.js';
 /**
  * ISBN input field
  */
